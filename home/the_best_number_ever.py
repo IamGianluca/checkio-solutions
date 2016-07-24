fb97dc6f5e5b4ea91aa7f4a3cf95e05d7f1559b0
Gianluca Rossi <gianluca.rossi@farfetch.com>
2016-07-24 20:04:53 +0100

    remove number from module name

---
+++ home/the_best_number_ever.py
@@ -0,0 +1,8 @@
+def checkio():
+    """
+    """
+    return 73  # if you are Sheldon
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(checkio(), (int, float, complex))
